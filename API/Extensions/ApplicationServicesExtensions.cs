458f0ff09347d7351605977765b48d2c12b02b30
Pum Suan Khai SEKTAK <josephkhaipi@gmail.com>
2021-09-07 16:50:35 +1000

    End of Section Five

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using API.Errors;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+
+                options.InvalidModelStateResponseFactory = ActionContext =>
+                {
+                    var error = ActionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Error = error
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                };
+
+            });
+
+            return services;
+
+        }
+    }
+}
